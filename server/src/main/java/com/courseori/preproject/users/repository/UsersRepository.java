500182acf38331e203665db88525077394a276a1
Eunjoo Lee <isej0916@gmail.com>
2022-09-08 13:59:45 +0900

    For uploading

--- server/src/main/java/com/courseori/preproject/users/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.courseori.preproject.users.repository;
-
-import com.courseori.preproject.users.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
